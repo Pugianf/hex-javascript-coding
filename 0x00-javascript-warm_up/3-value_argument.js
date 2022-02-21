362b5219f958af4f812837c3c87d24f9c965699c
Pugianf <pugianf@gmail.com>
2022-02-21 15:27:32 -0300

    update

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -4,8 +4,8 @@ const process = require('process');
 
 const args = process.argv;
 
-if (args[2] === false) {
-  console.log('No argument');
+if (args[2] === true) {
+  console.log(args[2]);
 } else {
-    console.log(args[2]);
+  console.log('No argument');
 }
