f1c9e36415e642e9d7715f501a290d872858c510
Pugianf <pugianf@gmail.com>
2022-02-21 20:58:15 -0300

    update

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -6,11 +6,15 @@ const args = process.argv;
 
 const x = parseInt(args[2]);
 
+let array;
+
 if (x) {
   for (let a = 0; a < x; a++) {
+    array = [];
     for (let i = 0; i < x; i++) {
-      console.log('X');
+      array.push('X');
     }
+    console.log(array.join(''));
   }
 } else {
   console.log('Missing size');
