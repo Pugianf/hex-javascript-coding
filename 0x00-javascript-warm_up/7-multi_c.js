b14c4b6edb2d422d8e371e5eb16880e9e61cccf4
Pugianf <pugianf@gmail.com>
2022-02-21 20:31:24 -0300

    update

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -7,9 +7,9 @@ const args = process.argv;
 const x = parseInt(args[2]);
 
 if (x) {
-  for (let i = 0; i <= x; i++) {
-  console.log('C is fun');
-}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 } else {
   console.log('Missing number of occurrences');
 }
