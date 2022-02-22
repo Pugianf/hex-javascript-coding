4e3e31207f45f7e5c5b7c0803c54e5fd3680c22f
Pugianf <pugianf@gmail.com>
2022-02-22 09:52:43 -0300

    update

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,13 @@ const args = process.argv.slice(2);
 
 let argsInt = [];
 
+function sortNum (a, b) {
+  return a - b;
+}
+
 if (args.length <= 1) {
   console.log(0);
 } else {
-  argsInt = args.sort(function (a, b) { return b-a };);
+  argsInt = args.sort(sortNum);
   console.log(argsInt[1]);
 }
